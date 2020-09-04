f23965340e4e4034cdfd94d45e9dadd0fd84892d
Daniel Koch <daniel@dkprog.com>
2020-09-04 08:40:57 -0300

    Added some testing.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dkprog/hello-go-module
 
 go 1.15
+
+require github.com/google/go-cmp v0.5.2
--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dkprog/hello-go-module/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println(morestrings.ReverseRunes("!dlrow ,olleH"))
+	fmt.Println(cmp.Diff("Hello, World!", "Hello, world!"))
+
 }
